2ee10230f168bae8b6c30c8fd197d8bdea2218d8
OksanaGukova <kseniagukova@ukr.net>
2024-06-26 23:10:31 +0300

    update

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,8 +1,10 @@
-export default function Filter({value, onFilter}) {
+import css from './SearchBox.module.css'
+
+export default function Filter({ value, onFilter }) {
     return (
-      <div>
-        <p>Find contact by name</p>
-        <input type="text" value={value} onChange={e => onFilter(e.target.value)} />
+      <div className={css.container}>
+        <p className={css.text}>Find contact by name</p>
+        <input className={css.input} type="text" value={value} onChange={e => onFilter(e.target.value)} />
       </div>
     );
     
